06cb54fae980c2ce67fe5e3e5ac699038af3b2aa
mmustafaseker <mustiseker.06@gmail.com>
2021-05-13 16:44:51 +0300

    Add project files.

---
+++ MVCOnlineTicariOtomasyon/Models/Siniflar/SatisHaraket.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace MVCOnlineTicariOtomasyon.Models.Siniflar
+{
+    public class SatisHaraket
+    {
+        [Key]
+        public int SatisId { get; set; }
+        //ürün
+        //cari
+        //personel
+        public DateTime Tarih { get; set; }
+        public int Adet { get; set; }
+        public decimal Fiyat { get; set; }
+        public decimal ToplamTutar { get; set; }
+        public Urun Urun { get; set; }
+        public Cariler Cariler { get; set; }
+        public Personel Personel { get; set; }
+    }
+}
