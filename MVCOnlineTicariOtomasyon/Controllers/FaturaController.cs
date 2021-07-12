51c3fc6f964b74fe0aafcd62d5e78c76d434fea2
mmustafaseker <mustiseker.06@gmail.com>
2021-07-12 15:28:33 +0300

    Fatura Controller

---
+++ MVCOnlineTicariOtomasyon/Controllers/FaturaController.cs
@@ -0,0 +1,20 @@
+﻿using MVCOnlineTicariOtomasyon.Models.Siniflar;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace MVCOnlineTicariOtomasyon.Controllers
+{
+    public class FaturaController : Controller
+    {
+        Context db = new Context();
+        // GET: Fatura
+        public ActionResult Index()
+        {
+            var liste = db.Faturalars.ToList();
+            return View(liste);
+        }
+    }
+}
